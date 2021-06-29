f2306cefeae20133148b07a54e5483fac9c50f9c
martin maguire <martin.maguire89@hotmail.co.uk>
2021-06-29 09:29:47 +0100

    background image added to home screen

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="home" id="background-image">
+      <div className="container">
+        <Link to="/Photos">Enter</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
